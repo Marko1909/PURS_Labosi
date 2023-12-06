683ea2260520a1f007633a39cd37060784a9c876
Marko <mantolic@tvz.hr>
2023-12-06 18:48:34 +0100

    Labosi6

--- Baze_podataka/upiti.sql
+++ Baze_podataka/upiti.sql
@@ -6,7 +6,9 @@ INNER JOIN ovlasti ON korisnik.id_ovlasti = ovlasti.id;
 
 SELECT * FROM korisnik WHERE username = 'kkolar' AND password = '12ab';
 
-SELECT ime, prezime, vrijednost AS vrijednost_temeprature FROM korisnikove_temperature
-INNER JOIN korisnik ON korisnikove_temperature.id_korisnika = korisnik.id
-INNER JOIN temperatura ON korisnikove_temperature.id_temperature = temperatura.id
+SELECT ime, prezime, vrijednost AS vrijednost_temprature FROM korisnikove_temperature
+LEFT JOIN korisnik ON korisnikove_temperature.id_korisnika = korisnik.id
+LEFT JOIN temperatura ON korisnikove_temperature.id_temperature = temperatura.id
 WHERE korisnik.id = 1;
+
+
