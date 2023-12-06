6af1aa2f49369f157a7c363493aa3b0d74e77f53
Marko <mantolic@tvz.hr>
2023-12-06 18:04:55 +0100

    Labosi6

---
+++ upiti.sql
@@ -0,0 +1,12 @@
+SELECT * FROM temperatura;
+SELECT * FROM temperatura ORDER BY id DESC LIMIT 1;
+
+SELECT ime, prezime, naziv AS naziv_ovlasti FROM korisnik
+INNER JOIN ovlasti ON korisnik.id_ovlasti = ovlasti.id;
+
+SELECT * FROM korisnik WHERE username = 'kkolar' AND password = '12ab';
+
+SELECT ime, prezime, vrijednost AS vrijednost_temeprature FROM korisnikove_temperature
+INNER JOIN korisnik ON korisnikove_temperature.id_korisnika = korisnik.id
+INNER JOIN temperatura ON korisnikove_temperature.id_temperature = temperatura.id
+WHERE korisnik.id = 1;
